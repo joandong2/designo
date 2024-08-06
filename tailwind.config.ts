fd1f0918753571c5b5fd71a6b47e444cefa91659
joandong2 <johnoblenda@gmail.com>
2024-08-06 15:20:11 -0400

    working on pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				"j-peach": "#E7816B",
+				"j-black": "#1D1C1E",
+				"j-white": "#FFFFFF",
+				"j-lightPeach": "#FFAD9B",
+				"j-darkGrey": "#333136",
+				"j-lightGrey": "#F1F3F5",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
